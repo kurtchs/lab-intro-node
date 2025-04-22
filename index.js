facf72a2ddda12cc83dc11294cac048546ad6cbb
“Kurt <chaconsaavedrakurt@gmailcomgit config --global user.name Kurt Chacongit config --global user.email chaconsaavedrakurt@gmailcom>
2025-04-22 19:52:20 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this .items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+    if(pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...this.items)
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.min(...this.items)
+  }
+
+  sum() {
+    return this.items.reduce((acc, curr) => acc + curr, 0)
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items.reduce((acc, curr) => acc + curr, 0) / this.items.length
+  }
 }
 
 module.exports = SortedList;
